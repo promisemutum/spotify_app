f7195a8b29c9d52fb82ed49f1d4e9b64beabdf98
Promise Mutum <problaze28@gmail.com>
2024-06-25 11:18:32 +0530

    first

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import customtkinter
 from dotenv import load_dotenv
 import os
+import base64
+from requests import post, get
+import json
 
 def build_gui():
     width_height = "800x500"
@@ -24,15 +27,38 @@ def build_gui():
 
 def button_click():
     song_name = entry.get()
+    search_func(song_name)
 
 def get_token():
+    global token
     load_dotenv()
     client_id = os.getenv("SPOTIFY_CLIENT_ID")
     client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
 
     auth_string = client_id +":"+client_secret
     auth_bytes = auth_string.encode("utf-8")
-    
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+    token_url = os.getenv("TOKEN_URL")
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+    result = post(token_url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+
+def search_func(song_name):
+    search_url = os.getenv("SEARCH_URL")
+    headers = {"Authorization": "Bearer "+token}
+    query = f"?q={song_name}&type=track&limit=1"
+    query_url = search_url + query
+    result = get(query_url, headers=headers)
+    json_result = json.loads(result.content)["tracks"]["items"][0]
+    if len(json_result)==0:
+        print("No songs found")
+        return None
+    return json_result
 
 def main():
     root = build_gui()
